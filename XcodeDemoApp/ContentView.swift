c85cf495748867f50338acbcf7530b32f34ec635
samsudin-sketch <sebastian.samsudin@gmail.com>
2025-09-11 02:31:28 +0200

    Initial Commit

---
+++ XcodeDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeDemoApp
+//
+//  Created by Sebastian Samsudin on 2025-09-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by Sebastian Samsudin on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
