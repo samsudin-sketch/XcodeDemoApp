d9ff016b86fed042c10e17de2fdec52450f2f8e0
samsudin-sketch <sebastian.samsudin@gmail.com>
2025-09-15 03:04:07 +0200

    skapade en modell, gjorde knappar i andra vyn (recept) samt gjorde en tredje vy

--- XcodeDemoApp/ContentView.swift
+++ XcodeDemoApp/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+//startvyn
 struct ContentView: View {
     var body: some View {
         NavigationStack {
@@ -28,17 +29,68 @@ struct ContentView: View {
     }
 }
 
+//"Andra" Vyn
 struct RecipesListView: View { // ej klar
+    let dummyRecipes = [//dummyRecipes är en array (lista) av Recipe-objekt.
+        Recipe(id: 1, title: "Pizza", author: "Anna", description: "Tomat och ost"),
+        Recipe(id: 2, title: "Pasta", author: "Erik", description: "Med gräddsås")
+    ]
+    
+    
     var body: some View {
-        Text("Detta är receptlistan")
-            .font(.title)
-            .navigationTitle("Recept")
+        VStack {//spacing?
+            Text("Recept")
+                .font(.largeTitle)
+                .bold()
+            
+            ForEach(dummyRecipes) { recipe in            //kör koden inuti blocket för varje recept
+                NavigationLink(destination: RecipeDetailView( recipe: recipe)){
+                    VStack(alignment: .leading) {
+                        Text(recipe.title)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        Text("av \(recipe.author)")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+        }
     }
 }
 
 
 
+//gör en modell (recipe)
+
+struct Recipe: Identifiable {//Identifiable möjliggör att användning i ForEach
+    let id: Int
+    let title: String
+    let author: String
+    let description: String
+}
+
+
 
 #Preview {
     ContentView()
+}
+
+//tredje vyn - RecipeDetailView som är en detaljvy
+struct RecipeDetailView: View {
+    let recipe: Recipe //En konstant variabel som är ett objekt av typen Recipe
+    
+    var body: some View {
+        VStack (alignment: .leading, spacing: 10){
+            Text(recipe.title)
+                .font(.largeTitle)
+            Text("av \(recipe.author)")
+                .font(.title2)
+                .foregroundColor(.gray)
+            Divider()
+            Text(recipe.description)
+                .padding(.top)
+        }
+        .navigationTitle("Detaljer")
+    }
 }
